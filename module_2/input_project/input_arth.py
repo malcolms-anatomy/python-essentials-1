126f6e5888773051f285b8069fe6669f2f680760
malcolm-ihere <miheremelam@mapis4u.com>
2025-07-16 01:35:20 +0100

    Create input_arth.py file

---
+++ module_2/input_project/input_arth.py
@@ -0,0 +1,19 @@
+# input a float value for variable a below
+a = float(input("Type a value here: "))
+
+# input a float value for variable b below
+b = float(input("Type the second value here: "))
+
+# output the result of addition below
+print("The sum of", a, "and", b, "is =", a+b)
+
+# output the result of subtraction below
+print("Subtracting", b, "from", a, "is =", a-b)
+
+# output the result of multiplication below
+print("Multiplication of", a, "and", b, "is =", a*b)
+
+# output the result of division below
+print("Dividing", a, "by", b, "is =", a/b)
+
+print("\nThat's all, folks!")
